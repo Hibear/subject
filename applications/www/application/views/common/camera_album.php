a0c0a790f9161af8c79adbf1fd182a945d6a9ba4
yonghua <254274509@qq.com>
2017-03-27 14:44:43 +0800

    修改camera

--- applications/www/application/views/common/camera_album.php
+++ applications/www/application/views/common/camera_album.php
@@ -14,19 +14,19 @@
         ]
     });
 
-   // wx.ready(function () {
-     //   wx.onMenuShareTimeline({
-       //     title: "<?php echo $title;?>",
-         //   link: "<?php echo $link;?>",
-           // imgUrl: "<?php echo $imgUrl;?>",
-            //desc: "<?php echo $desc;?>",
-            //success: function () {
+    wx.ready(function () {
+        wx.onMenuShareTimeline({
+            title: "<?php echo $title;?>",
+            link: "<?php echo $link;?>",
+            imgUrl: "<?php echo $imgUrl;?>",
+            desc: "<?php echo $desc;?>",
+            success: function () {
                 //   alert("分享成功");
-            //},
-            //cancel: function () {
+            },
+            cancel: function () {
                 //alert("取消分享");
-            //}
-        //});
+            }
+        });
 
         $("#add_pic").click(function () {
             wx.chooseImage({
@@ -35,7 +35,17 @@
                 sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
                 success: function (res) {
                     var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
-                }
+              		wx.uploadImage(
+           			 localId: localIds[0], // 需要上传的图片的本地ID，由chooseImage接口获得
+           			 isShowProgressTips: 1, // 默认为1，显示进度提示
+            			 success: function (res) {
+                		 var serverId = res.serverId; // 返回图片的服务器端ID
+				console.log(serverId);
+            }
+        });
+
+
+		  }
             });
         });
 
