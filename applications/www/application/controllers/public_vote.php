878162e99501e8fe5a848b1144112ac1dba8e8ac
root <254274509@qq.com>
2017-04-20 10:03:41 +0800

    简单部署手机端投票控制器

---
+++ applications/www/application/controllers/public_vote.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+class Public_vote extends MY_Controller{
+	
+    public function __construct(){
+        parent::__construct();
+    }
+    
+    /**
+     * 投票首页
+     */
+    public function index(){
+        //登陆
+        //获取本次活动的基本信息
+        //查询所有参选对象以及基本信息
+    }
+    
+    public function vote(){
+        
+    }
+}
