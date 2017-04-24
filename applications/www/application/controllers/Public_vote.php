b7f76672579620ac1c642bd264039c967b377829
root <254274509@qq.com>
2017-04-24 16:50:28 +0800

    初步完成公共投票系统

---
+++ applications/www/application/controllers/Public_vote.php
@@ -0,0 +1,152 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+class Public_vote extends MY_Controller{
+    private $app;
+    public function __construct(){
+        parent::__construct();
+        $this->load->model(array(
+                'Model_weixin_active' => 'Mweixin_active',
+                'Model_vote_obj' => 'Mvote_obj',
+                'Model_active_vote_log' => 'Mactive_vote_log'
+        ));
+        //分享用的
+        $this->app = C("appid_secret.dashi");
+        $this->load->driver('cache');
+    }
+    
+    public function index(){
+        
+        $data = $this->data;
+        $active_id = (int) $this->input->get('active_id');
+        if(!$active_id){
+            show_404();
+        }
+        $this->check_login($active_id);
+        
+        $info = $this->cache->file->get('vote_'.$active_id);
+        $vote_obj = trim($this->input->get('vote_obj'));
+        if(!$info){
+            $info = $this->Mweixin_active->get_one('*', ['id' => $active_id, 'is_del' => 0, 'type' => C('active_type.tp.id')]);
+            if(!$info){
+                show_404();
+            }else{
+                $this->cache->file->save('vote_'.$active_id, $info, 5*60); //缓存5分钟
+            }
+        }
+        
+        $data['info'] = $info;
+        $data['total'] = $this->Mvote_obj->get_lists('sum(score) as total', ['active_id' => $active_id, 'is_del' => 0])[0]['total'];
+        $where = ['active_id' => $active_id, 'is_del' => 0 ];
+        if($vote_obj){
+            $where['like'] =[
+                'vote_obj' => $vote_obj
+            ];
+        }
+        $data['lists'] = $this->Mvote_obj->get_lists('id,title,cover_img,vote_obj,score', $where, ['create_time' => 'desc']);
+        $this->load->view('public_vote/index', $data);
+    }
+    
+    /**
+     * 给投票对象投票
+     */
+    public function add_vote(){
+        $data = $this->data;
+        $user_info = $this->session->userdata('user_info');
+        if(!$user_info){
+            $this->return_json(['code' => 0, 'msg' => '请先登陆！']);
+        }
+        $active_id = (int) $this->input->post('active_id');
+        $obj_id = (int) $this->input->post('obj_id');
+        $info = $this->cache->file->get('vote_'.$active_id);
+        if(!$info){
+            $info = $this->Mweixin_active->get_one('*', ['id' => $active_id, 'is_del' => 0, 'type' => C('active_type.tp.id')]);
+            if(!$info){
+                $this->return_json(['code' => 0, 'msg' => '活动不存在']);
+            }else{
+                $this->cache->file->save('vote_'.$active_id, $info, 5*60); //缓存5分钟
+            }
+        }
+        
+        //判断当前时间是否在活动时间内
+        $time = time();
+        $start_time = $info['start_time'];
+        $no_start_msg = $info['no_start_msg'];
+        $end_msg = $info['end_msg'];
+        $end_time = $info['end_time'];
+        $this->check_active_time($time, $start_time, $end_time, $no_start_msg, $end_msg);
+        
+        //判断今天是否能够投票
+        $openid = $user_info['openid'];
+        $this->check_today_and_vote($active_id, $openid, $obj_id, $info['count']);
+    }
+    
+    /**
+     * 判断当前时间能否进行投票
+     * @param  $time 当前时间
+     * @param  $start 活动开始时间
+     * @param  $end 活动结束时间
+     * @param  $no_start_msg 未开始的提示
+     * @param  $end_msg 已结束的提示
+     */
+    private function check_active_time($time, $start, $end, $no_start_msg, $end_msg){
+    
+        if($time < strtotime($start)){
+            $this->return_json(['code' => 0, 'msg' => $no_start_msg]);
+        }else if($time >= strtotime($end)){
+            $this->return_json(['code' => 0, 'msg' => $end_msg]);
+        }
+    }
+    
+    private function check_today_and_vote($openid, $active_id, $obj_id, $counts){
+        //判断是否投票过今日允许的范围
+        $count = $this->Mactive_vote_log->count(['create_time' => date('Y-m-d'), 'active_id' => $active_id, 'openid' => $openid]);
+        if($count >= $counts){
+            $this->return_json(['code' => 0, 'msg' => '今日投票次数超限！']);
+        }
+        unset($count);
+        //判断今天是否对次对象投票过
+        $count = $this->Mactive_vote_log->count(['create_time' => date('Y-m-d'), 'active_id' => $active_id, 'obj_id' => $obj_id, 'openid' => $openid]);
+        if($count){
+            $this->return_json(['code' => 0, 'msg' => '你已经对此透过票了！']);
+        }
+        
+        //开始添加投票记录
+        $add = [
+            'active_id' => $active_id,
+            'openid' => $openid,
+            'obj_id' => $obj_id,
+            'score' => 1,
+            'create_time' => date('Y-m-d')
+        ];
+        $res = $this->Mactive_vote_log($add);
+        if($res){
+            //涨分
+            $id = $res;
+            unset($res);
+            $up =[
+                'incr' => [
+                    '`score`' => 1
+                ]
+            ];
+            $res = $this->Mvote_obj->update_info($up, ['id' => $obj_id, 'active_id' => $active_id]);
+            if($res){
+                $this->return_json(['code' => 0, 'msg' => '投票成功！']);
+            }else{
+                $this->Mactive_vote_log->delete(['id' => $id]);
+                $this->return_json(['code' => 0, 'msg' => '请重试！']);
+            }
+        }else{
+            $this->return_json(['code' => 0, 'msg' => '请重试！']);
+        }
+        
+    }
+    
+    private function check_login($id = 0){
+        $user_info = $this->session->userdata('user_info');
+        if(!$user_info){
+            $this->session->set_userdata('login_back_url', '/public_vote/index?active_id='.$id);
+            redirect(C('domain.h5.url').'/weixin_login/login');
+            exit;
+        }
+    }
+}
---
+++ applications/www/application/views/public_vote/index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" type="text/css" href="<?php echo get_css_js_url('gold/reset.css', 'www')?>" media="all" />
+    <link rel="stylesheet" type="text/css" href="<?php echo get_css_js_url('public_vote/style.css', 'h5')?>" media="all" />
+    <title><?php echo $info['title']?></title>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+    <meta content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport">
+    <meta name="Keywords" content="" />
+    <meta name="Description" content="" />
+    <!-- Mobile Devices Support @begin -->
+    <meta content="application/xhtml+xml;charset=UTF-8" http-equiv="Content-Type">
+    <meta content="no-cache,must-revalidate" http-equiv="Cache-Control">
+    <meta content="no-cache" http-equiv="pragma">
+    <meta content="0" http-equiv="expires">
+    <meta content="telephone=no, address=no" name="format-detection">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta name="apple-mobile-web-app-capable" content="yes" /> <!-- apple devices fullscreen -->
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+</head>
+<body>
+    <div class="container center">
+        <div class="header">
+            <div class="h-title"><?php echo $info['title']?></div>
+            <div class="h-time"><?php echo date('Y-m-d', strtotime($info['start_time']));?></div>
+            <div class="h-coverimg"><img alt="" src="<?php echo get_img_url($info['cover_img']);?>"></div>
+            <div class="h-desc">
+                <span>投票说明：</span>
+                <?php echo $info['desc']?>
+            </div>
+        </div>
+        <div class="search">
+            <input placeholder="输入被投票的对象..." type="text" name="vote_obj"/>
+            <button>查找</button>
+        </div>
+        <div class="line"></div>
+        <div class="main">
+        <?php if(isset($lists)):?>
+        <?php foreach ($lists as $k => $v):?>
+            <div class="lists">
+                <div class="m-coverimg"><img alt="" src="<?php echo get_img_url($v['cover_img'])?>"></div>
+                <div class="m-title"><div class="m-radio" data="<?php echo $v['id']?>"></div><?php echo $v['vote_obj'].'-《'.$v['title'].'》'?></div>
+                <div class="m-score">
+                    <div class="jdt">
+                        <div style="width:<?php echo sprintf('%.2f', ($v['score']/$total)*100)?>%" class="jdz"></div>
+                    </div>
+                    &nbsp;<?php echo sprintf('%.2f', ($v['score']/$total)*100)?>%(<?php echo $v['score']?>票)
+                </div>
+            </div>
+            <?php endforeach;?>
+        <?php endif;?>
+        </div>
+    </div>
+    <footer class="footer">
+        <button class="button" id="vote" data="0" status="0">投票</button>
+    </footer>
+    <script src="<?php echo get_css_js_url('guaguaka/jquery-1.9.1.js', 'h5')?>"></script>
+    <script>
+        //选择
+        $('.m-radio').on('click', function(){
+            var _obj = $(this);
+            var id = _obj.attr('data');
+            //所有同胞失去焦点
+            $('.m-radio').removeClass('act');
+            _obj.addClass('act');
+            //激活投票按钮
+            $('#vote').attr('data', id);
+            $('#vote').attr('status', 1);
+            $('#vote').addClass('active');
+        })
+        //投票
+        $('#vote').on('click', function(){
+            status = $(this).attr('status');
+            if(status == 1){
+                var obj_id = $(this).attr('data');
+                var active_id = <?php echo $info['id']?>;
+                $.post('/public_vote/add_vote', {'obj_id':obj_id, 'active_id':active_id}, function(data){
+                    if(data){
+                        if(data.code == 1){
+                        	alert(data.msg);
+                        }else{
+                        	alert(data.msg);
+                        }
+                    }else{
+                        alert('网络异常')
+                    }
+                })
+            }
+        });
+    </script>
+</body>
+</html>
