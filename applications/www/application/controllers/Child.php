d42b6782ba51c78fb697cad0500b9e0556ee860d
root <254274509@qq.com>
2017-05-24 10:59:34 +0800

    添加了微信登陆

--- applications/www/application/controllers/Child.php
+++ applications/www/application/controllers/Child.php
@@ -4,17 +4,23 @@ class Child extends MY_Controller{
     private $app;
     public function __construct(){
         parent::__construct();
-        $this->load->model(array(
-
-        ));
         //分享用的
         $this->app = C("appid_secret.dashi");
-        $this->load->driver('cache');
     }
     
     public function index(){
         $data = $this->data;
+        $this->check_login();
         $this->load->view('child/index', $data);
+    }
+    
+    private function check_login(){
+        $user_info = $this->session->userdata('user_info');
+        if(!$user_info){
+            $this->session->set_userdata('login_back_url', '/child/index');
+            redirect(C('domain.h5.url').'/weixin_active_login/login');
+            exit;
+        }
     }
     
 }
